9ce33e59d8f43a928090f5ec8cbdfce74b88649e
Marcos Rufino <marcosrufino@live.com>
2020-08-07 11:36:02 -0300

    adicionado statistics no hibernate

--- src/main/java/com/apache/camel/demo/apachecamel/converter/ConverterRoute.java
+++ src/main/java/com/apache/camel/demo/apachecamel/converter/ConverterRoute.java
@@ -10,13 +10,16 @@ import com.apache.camel.demo.apachecamel.rest.model.Employer;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.ThreadPoolBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.component.jackson.ListJacksonDataFormat;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Component
 public class ConverterRoute extends RouteBuilder {
-
 
     @Override
     public void configure() throws Exception {
@@ -25,6 +28,11 @@ public class ConverterRoute extends RouteBuilder {
         context.getTypeConverterRegistry().addTypeConverters(new EmployerTypeConverter());
 
         JacksonDataFormat orderFormat = new ListJacksonDataFormat(OrderFull.class);
+        //Thread exemplo 01
+        //ExecutorService threadPool = new ThreadPoolBuilder(context).poolSize(50).maxPoolSize(60).maxQueueSize(500).build("CustomThreadPool");
+        //Thread exemplo 02
+        //ExecutorService threadPool = Executors.newFixedThreadPool(20);
+
 
         from("timer:rest?period=10s")
                 .to("direct:process-service");
@@ -36,16 +44,22 @@ public class ConverterRoute extends RouteBuilder {
                 .unmarshal(orderFormat)
                 .split().body()
                 .multicast()
+                //Thread exemplo 03
+                .parallelProcessing()
+                //##Thread exemplo 01 e 02
+                //.threads().executorService(threadPool)
                 .to("direct:order", "direct:employer")
                 .log("${body}")
         ;
 
         from("direct:order")
                 .convertBodyTo(Order.class)
-                .bean(OrderBean.class);
+                .bean(OrderBean.class)
+                .log("Reposta : Thread: ${threadName}");
 
         from("direct:employer")
                 .convertBodyTo(Employer.class)
-        .bean(UserBean.class, "saveUser");
+        .bean(UserBean.class, "saveUser")
+                .log("Reposta : Thread: ${threadName}");;
     }
 }
